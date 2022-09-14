7a53437ee84333a4a7d26d33b3f33120cffb5e3a
AmauriBlanco <blancoamauri@gmail.com>
2022-09-14 18:51:22 -0300

    aula 4

---
+++ app.ts
@@ -0,0 +1,9 @@
+let nome: string;
+let sobrenome: string;
+let idade: number;
+
+nome = "Maria";
+sobrenome = "Blanco";
+idade = 36;
+
+console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
---
+++ dist/app.js
@@ -0,0 +1,8 @@
+"use strict";
+let nome;
+let sobrenome;
+let idade;
+nome = "Amauri";
+sobrenome = "Blanco";
+idade = 31;
+console.log(`nome: ${nome} ${sobrenome}, idade: ${idade}`);
